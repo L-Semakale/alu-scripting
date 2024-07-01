58fc748fcac978dc91400143c25e18d7b43bd7b4
L-Semakale <l.semakale@alustudent.com>
2024-07-01 19:50:23 +0000

    checking num of subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-""" 0-subs.py """
+"""
+Module to fetch the number of subscribers for a given subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ number_of_subscribers """
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
