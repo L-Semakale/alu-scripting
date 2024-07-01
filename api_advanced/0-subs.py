1f6a79fb4e5567f7695eb1b12fa50cee5f47f6ee
L-Semakale <l.semakale@alustudent.com>
2024-07-01 19:43:56 +0000

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,12 +5,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ number_of_subscribers """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         return response.json()["data"]["subscribers"]
     else:
         return 0
 
+# Sample usage:
+if __name__ == "__main__":
+    subreddit = "python"
+    print(number_of_subscribers(subreddit))
