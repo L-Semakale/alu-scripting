05d87e1106edf3e22d30658701d05fabd3dca96b
L-Semakale <l.semakale@alustudent.com>
2024-07-03 10:58:56 +0000

    modifying the file

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,19 +9,20 @@ def number_of_subscribers(subreddit):
     subreddit (str): The name of the subreddit.
 
     Returns:
-    int: The number of subscribers.
+    int: The number of subscribers, or 0 if the subreddit does not exist.
     """
     user_agent = "Mozilla/5.0 (x11; Linux x86_64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-    URL = f"https://api.reddit.com/r/{subreddit}/about"
+    url = f"https://api.reddit.com/r/{subreddit}/about"
 
     headers = {"User-Agent": user_agent}
 
-    response = requests.get(URL, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
 
     data = response.json()
     return data['data']['subscribers']
 
-    # Example usage
-    print(number_of_subscribers('python'))
+# Example usage
+print(f"Subscribers in 'python': {number_of_subscribers('python')}")
+print(f"Subscribers in 'nonexistentsubreddit': {number_of_subscribers('nonexistentsubreddit')}")
