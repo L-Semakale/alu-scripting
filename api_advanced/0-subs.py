012be98a3760b69e5654cfbddfb83e00e6ce2de9
L-Semakale <l.semakale@alustudent.com>
2024-07-04 23:01:35 +0000

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,37 +1,12 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+0-main
 """
-import requests
+import sys
 
-
-def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-
-    Returns:
-        int: The number of subscribers for the subreddit. Returns 0 if the subreddit
-             is invalid or if an error occurs.
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        'User-Agent': 'linux:alu-scripting:v1.0.0 (by /u/Mpho_19)'
-    }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get("data", {}).get("subscribers", 0)
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
-    except ValueError:
-        return 0
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
