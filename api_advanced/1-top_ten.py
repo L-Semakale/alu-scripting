5d8fa8bbe819a8c0ee7a15d77a79d5170f42fd90
L-Semakale <l.semakale@alustudent.com>
2024-07-04 23:52:36 +0000

    printing the titles of the first 10 hot posts listed

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-"""" Top Ten Limit"""
+"""
+This module contains a function that queries the Reddit API and 
+returns the top 10 hot posts for a given subreddit.
+"""
 import requests
 
-
 def top_ten(subreddit):
-    """"top ten"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    my_headers = {
-        "User-Agent": "api-advanced/project"
-        }
+    """
+    Queries the Reddit API and prints the titles of
+    the top 10 hot posts for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit to query.
+    
+    Returns:
+        None: Prints the titles of the top 10 hot posts or
+        None if the subreddit is invalid.
+    """
+    # Construct the URL to fetch the top 10 hot posts from the subreddit
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    
+    # Set the User-Agent header to mimic a browser request
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    
+    try:
+        # Make a GET request to the Reddit API to fetch the top hot posts
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
 
-    response = requests.get(URL, headers=my_headers)
-    # raw_response = response.json()['data']['children']
+        # Parse the JSON response and extract the list of hot posts
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
 
-    if response.status_code != 200:
+        # Iterate over the list of hot posts and print the title of each post
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
+        # If any exception occurs, print None to indicate an error
         print(None)
-    else:
-        json_response = response.json()
-        posts = json_response['data']['children']
-        [print(post.get('data').get('title')) for post in posts]
